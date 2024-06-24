d4d9889288dc3a29277d04271f47c153a23e88ac
모준서 <165753364+Junseo-Mo@users.noreply.github.com>
2024-06-24 13:35:22 +0900

    [NC-296] CI/CD JIB 적용 (#2)
    * chore: JIB PlugIn 추가 및 Extension 설정
    
    Extension 설정은 기본적인 것들만 우선적으로 설정
    
    * chore: JIB 으로 Image build 방식 변경, JDK:21 로 변경, AWS variable refactor
    
    * chore: EOF 처리
    
    * chore: JDK version 변경 누락 수정
    
    * chore: 개발배포환경(alpha) 과 일치하도록 파일명 변경
    
    * chore: Timezone 설정변경 UTC -> KST
    
    * chore: multi module build 설정 In CI
    
    * chore: space code 추가
    
    ---------
    
    Co-authored-by: jayking <mojunseo49@naver.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+    id("com.google.cloud.tools.jib") version "3.4.3"
     kotlin("plugin.jpa") version "1.9.24"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
@@ -47,5 +48,37 @@ subprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+}
+
+jib {
+    val env = System.getenv("DEPLOY_ENVIRONMENT")
+    from {
+        image = "amazoncorretto:21"
+    }
+    to {
+        image = "${System.getenv("AWS_ECR_REGISTRY")}/${System.getenv("AWS_ECR_REPOSITORY_NAME")}:${System.getenv("IMAGE_TAG")}"
+    }
+    container {
+        jvmFlags = when (env) {
+            "alpha" -> mutableListOf(
+                "-Xms512m",
+                "-Xmx1024m",
+                "-Dspring.profiles.active=$env",
+                "-Duser.timezone=Asia/Seoul",
+                "-XX:+UseContainerSupport"
+            )
+            "staging" -> mutableListOf(
+                "-Xms1024m",
+                "-Xmx2048m",
+                "-Dspring.profiles.active=$env",
+                "-Duser.timezone=Asia/Seoul",
+                "-XX:+UseContainerSupport"
+            )
+            else -> emptyList()
+        }
+        creationTime = "USE_CURRENT_TIMESTAMP"
+        ports = mutableListOf("8080")
+        mainClass = "org.colosseum.sample.SampleApplication"
     }
 }
